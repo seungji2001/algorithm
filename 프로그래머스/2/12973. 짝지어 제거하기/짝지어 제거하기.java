d444621d613d5fa14eaed5ecf28420fc824daf70
seungji2001 <86452494+seungji2001@users.noreply.github.com>
2024-09-18 00:30:52 +0900

    [level 2] Title: 짝지어 제거하기, Time: 86.46 ms, Memory: 72.8 MB -BaekjoonHub

--- 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.java
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.java
@@ -3,20 +3,19 @@ import java.util.Stack;
 class Solution {
     public int solution(String s) {
         Stack<Character> stack = new Stack<>();
-        
-        // 문자열을 순차적으로 처리
-        for (int i = 0; i < s.length(); i++) {
-            char current = s.charAt(i);
-            
-            // 스택이 비어있지 않고, 현재 문자가 스택의 최상단 문자와 같으면
-            if (!stack.isEmpty() && stack.peek() == current) {
-                stack.pop(); // 쌍을 이루는 문자를 제거
-            } else {
-                stack.push(current); // 그렇지 않으면 스택에 추가
+        for(char c : s.toCharArray()){
+            if(!stack.isEmpty()){
+                if(c == stack.peek()){
+                    stack.pop();
+                    continue;
+                }
             }
+            stack.push(c);
         }
         
-        // 스택이 비어있으면 모든 문자가 쌍을 이루어 제거된 상태
-        return stack.isEmpty() ? 1 : 0;
+        if(stack.isEmpty()){
+            return 1;
+        }
+        return 0;
     }
 }
